3d99ad815a36766ca1faea902df0d6cb8ec8c478
Paulina <34506586+Czudo@users.noreply.github.com>
2019-04-07 22:16:20 +0200

    final version of random walk and result

--- lab4/randomWalk.py
+++ lab4/randomWalk.py
@@ -9,14 +9,15 @@ from matplotlib import patches
 def initRandomWalk(n, N):
     G = nx.grid_graph(dim=[N, N])
     nodes = list(G.nodes())
-    [x, y] = list(zip(*nodes))  #
+    [x, y] = list(zip(*nodes))
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(111, aspect='equal')
-    spacing = 1
-    minorLocator = MultipleLocator(spacing)
-    ax1.xaxis.set_minor_locator(minorLocator)
+
+    minorLocator = MultipleLocator(1)  # space between lines in grid
+    ax1.xaxis.set_minor_locator(minorLocator)  # set that on x and y axis
     ax1.yaxis.set_minor_locator(minorLocator)
     ax1.grid(which='minor', axis='both')
+
     plt.xlim([min(x)-0.5, max(x)+0.5])
     plt.ylim([min(y)-0.5, max(y)+0.5])
     start = (round(N/2), round(N/2))  # start at middle of plot
@@ -30,41 +31,39 @@ def RandomWalk(n, fig1, ax1, G, start):
     )
     fig1.savefig('randomWalk_{}_{}.png'.format(N, 0))
     for i in range(1, n+1):
-
         ax1.add_patch(
-            patches.Circle(start, 0.11, color='w')  # cover starting position with white dot
+            patches.Circle(start, 0.15, color='w')  # cover starting position with white dot
         )
 
-        listOfNext = list(G.neighbors(start))
+        posibleVert = list(G.neighbors(start))
+        next = random.choice(posibleVert)
 
-        index = random.randint(0, len(listOfNext)-1)
-        next = listOfNext[index]
         ax1.add_patch(
             patches.Circle(next, 0.1, color='r')  # add dot at actual position on plot
         )
-        colors = nx.get_edge_attributes(G, 'color')
+        colors = nx.get_edge_attributes(G, 'color')  # get color attribute for all edges
 
-        if (start, next) in list(colors.keys()):
-            if colors[(start, next)] != 0:
-                color = colors[(start, next)]-20
+        if (start, next) in list(colors.keys()):  # if connection was visited
+            if colors[(start, next)] != 0:  # and if its color is not black
+                color = colors[(start, next)]-20  # set darker color
                 nx.set_edge_attributes(G, {(start, next): {'color': color}})
-            else:
-                color = colors[(start, next)]
+            else:  # if edges color is black
+                color = colors[(start, next)]  # set the same color
                 nx.set_edge_attributes(G, {(start, next): {'color': color}})
 
-        elif (next, start) in list(colors.keys()):
-            if colors[(next, start)] != 0:
+        elif (next, start) in list(colors.keys()):  # else if connection was visited, but from next to start
+            if colors[(next, start)] != 0:  # do the same
                 color = colors[(next, start)]-20
                 nx.set_edge_attributes(G, {(next, start): {'color': color}})
             else:
                 color = colors[(next, start)]
                 nx.set_edge_attributes(G, {(next, start): {'color': color}})
-        else:
-            color = 120
+        else:  # if its firs visit from start to next (or from next to start)
+            color = 120  # set light grey
             nx.set_edge_attributes(G, {(start, next): {'color': color}})
-
+        # plot line between nodes with its color
         ax1.plot([start[0], next[0]], [start[1], next[1]], color=(color/255, color/255, color/255))
-        fig1.savefig('randomWalk_{}_{}.png'.format(N, i))
+        fig1.savefig('randomWalk_{}_{}.png'.format(N, i))  # save current plot
         start = next
 
     files = ['randomWalk_{}_{}.png'.format(N, i) for i in range(0, n+1)]
@@ -74,6 +73,6 @@ def RandomWalk(n, fig1, ax1, G, start):
 
 
 if __name__ == '__main__':
-    N = 20
-    n = 200
+    N = 20  # number of
+    n = 2000  # number of steps
     initRandomWalk(n, N)
