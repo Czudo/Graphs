a9c6db63c00c552074fc6b0f43f7b666cdba1df6
Paulina <34506586+Czudo@users.noreply.github.com>
2019-04-11 00:02:33 +0200

    final version (i think so)

--- lab4/PearsonsRandomGraph.py
+++ lab4/PearsonsRandomGraph.py
@@ -4,65 +4,99 @@ import random
 import math
 import collections
 import numpy as np
-
-def getCords(G):
-    cords = collections.OrderedDict(sorted(nx.get_node_attributes(G, 'cords').items())).values()
-    cords = list(zip(*cords))
-    return cords
+import subprocess
 
 
-def PearsonsRandomWalk(n, G):
+def getCoordinates(G):
+    coordinates = collections.OrderedDict(sorted(nx.get_node_attributes(G, 'coordinates').items())).values()
+    coordinates = list(zip(*coordinates))
+    return coordinates
+
+
+def PearsonsRandomWalk(G, N):
     start = 0
-    G.add_node(start, cords=[0, 0])
+    G.add_node(start, coordinates=[0, 0])
     x, y = 0, 0
 
-    for i in range(1, n+1):
+    for next in range(1, N+1):
         phi = random.random()*2*math.pi
         x = x + math.cos(phi)
         y = y + math.sin(phi)
-        G.add_node(i, cords=[x, y])
-        G.add_edge(start, i, angle=phi)
-        start = i
+        G.add_node(next, coordinates=[x, y])
+        G.add_edge(start, next, angle=phi)
+        start = next
 
-    cords = getCords(G)
-    plt.plot(cords[0], cords[1], '-o', linewidth=0.5, markersize=1)
-    return max(cords[0]), min(cords[0]), max(cords[1]), min(cords[1])
+    [x, y] = getCoordinates(G)
+    plt.plot(x, y, '-o', linewidth=0.2, markersize=0.5)
+    return max(x), min(x), max(y), min(y)  # return maximal and minimal coordinates to set x and y limits
 
 
-def distribution(G, N):
-    An = [0]*len(G)
-    print(An)
-    Bn = [0]*len(G)
-    for i in range(len(G)):
-        x,y = getCords(G[i])
-        An[i] = sum(1 for k in x if k > 0)
-        Bn[i] = sum(1 for k in range(len(x)) if x[k] > 0 and y[k] > 0)
+def distribution(G, N, n):
+    An = []
+    Bn = []
+    for i in range(n):
+        x, y = getCoordinates(G[i])
+        An.append(sum(1 for k in x if k > 0))
+        Bn.append(sum(1 for k in range(len(x)) if x[k] > 0 and y[k] > 0))
+
     plt.figure()
-    [unique, counts] = np.unique(np.sort(An), return_counts=True)
-    plt.plot(unique, counts)
-    plt.savefig('An_{}.png'.format(N))
-    print(Bn)
+    results, edges = np.histogram(np.asarray(An)/N, density=True)
+    binWidth = edges[1] - edges[0]
+    plt.bar(edges[:-1], results*binWidth, binWidth, label='empirical')
+    plt.plot([1/2, 1/2] , [0, max(results*binWidth)], 'r', label='theoretical mean')
+    plt.legend()
+    plt.xlabel(r'$A_n$')
+    plt.ylabel('PDF')
+    plt.savefig('pearsonsrandomwalk/prw_An_' + str(n) + 'graphs_' + str(N) + 'steps.png')
+
     plt.figure()
-    plt.hist(Bn, density=True, bins=10, label='empirical')
-    plt.savefig('Bn_{}.png'.format(N))
+    results, edges = np.histogram(np.asarray(Bn)/N, density=True)
+    binWidth = edges[1] - edges[0]
+    plt.bar(edges[:-1], results * binWidth, binWidth, label='empirical')
+    plt.plot([1 / 4, 1 / 4], [0, max(results * binWidth)], 'r', label='theoretical mean')
+    plt.legend()
+    plt.xlabel(r'$B_n$')
+    plt.ylabel('PDF')
+    plt.savefig('pearsonsrandomwalk/pwr_Bn_' + str(n) + 'graphs_' + str(N) + 'steps.png')
+
+
+def plotOneGraph(G, N):
+    [x, y] = getCoordinates(G)
+    fig1 = plt.figure()
+    ax1 = fig1.add_subplot(111, aspect='equal')
+    plt.xlim([min(x) - 0.5, max(x) + 0.5])
+    plt.ylim([min(y) - 0.5, max(y) + 0.5])
+    plt.xlabel('x')
+    plt.ylabel('y')
+    ax1.plot(x[0], y[0], '-o', color='blue', linewidth=0.2, markersize=0.5)
+
+    fig1.savefig('pearsonsrandomwalk/prw_' + str(N) + '_{0:04}.png'.format(0))
+    for i in range(N):
+        ax1.plot([x[i], x[i+1]], [y[i], y[i+1]], '-o', color='blue', linewidth=0.2, markersize=0.5)
+        fig1.savefig('pearsonsrandomwalk/prw_' + str(N) + '_{0:04}.png'.format(i+1))
+
+    cmd = ['magick', 'convert', '-delay', '20', '-loop', '0', 'pearsonsrandomwalk/prw_'
+           + str(N) + '_*.png', 'pearsonsrandomwalk/gif_prw_' + str(N) + 'steps.gif']
+    subprocess.call(cmd, shell=True)
 
 
 if __name__ == '__main__':
-    G = []
-    n = 2000
-    N = 1000
-    maxx, minx, maxy, miny = 0, 0, 0, 0
-    for i in range(0, n):
+    G = []  # list of graphs
+    n = 10  # number of graphs
+    N = 500  # number of steps
+    maxX, minX, maxY, minY = 0, 0, 0, 0
+    for i in range(n):
         G.append(nx.Graph())
-        tempmaxx, tempminx, tempmaxy, tempminy = PearsonsRandomWalk(N, G[i])
-        maxx = max(maxx, tempmaxx)
-        minx = min(minx, tempminx)
-        maxy = max(maxy, tempmaxy)
-        miny = min(miny, tempminy)
+        x1, x2, y1, y2 = PearsonsRandomWalk(G[i], N)
+        maxX = max(maxX, x1)
+        minX = min(minX, x2)
+        maxY = max(maxY, y1)
+        minY = min(minY, y2)
 
-    plt.plot([minx, maxx], [0, 0], ':', linewidth=1, color='black')
-    plt.plot([0, 0], [miny, maxy], ':', linewidth=1, color='black')
+    plt.plot([minX, maxX], [0, 0], ':', linewidth=1, color='black')
+    plt.plot([0, 0], [minY, maxY], ':', linewidth=1, color='black')
     plt.xlabel('x')
     plt.ylabel('y')
-    plt.savefig('PRandomWalk_{}_{}.png'.format(N, n))
-    distribution(G, N)
+    plt.savefig('pearsonsrandomwalk/prw_graphs_' + str(n) + '_' + str(N) + 'steps.png')
+    distribution(G, N, n)
+    plotOneGraph(G[0], N)
--- lab4/randomWalk.py
+++ lab4/randomWalk.py
@@ -27,13 +27,13 @@ def initRandomWalk(n, N):
 
 
 def RandomWalk(n, fig1, ax1, G, start):
-    ax1.add_patch(patches.Circle(start, 0.1, color='r'))  # add dot as start position
+    ax1.add_patch(patches.Circle(start, 0.1, color='r'))  # add dot at start position
     fig1.savefig('randomwalk/rw_' + str(N) + '_{0:04}.png'.format(0))
 
     for i in range(1, n+1):
         posibleVertices = list(G.neighbors(start))
         next = random.choice(posibleVertices)
-        ax1.add_patch(patches.Circle(start, 0.15, color='w'))  # cover starting position with white dot
+        ax1.add_patch(patches.Circle(start, 0.15, color='w'))  # cover old starting position with white dot
         ax1.add_patch(patches.Circle(next, 0.1, color='r'))  # add dot at actual position on plot
 
         colors = nx.get_edge_attributes(G, 'color')  # get color attribute for all edges
@@ -52,18 +52,16 @@ def RandomWalk(n, fig1, ax1, G, start):
 def getColor(G, colors, start, next):
     if (start, next) in list(colors.keys()):  # if connection was visited
         if colors[(start, next)] != 0:  # and if its color is not black
-            color = colors[(start, next)] - 20  # set darker color
-            nx.set_edge_attributes(G, {(start, next): {'color': color}})
+            color = colors[(start, next)] - 20
+            nx.set_edge_attributes(G, {(start, next): {'color': color}})  # set darker color
         else:  # if edges color is black
-            color = colors[(start, next)]  # set the same color
-            nx.set_edge_attributes(G, {(start, next): {'color': color}})
+            color = colors[(start, next)]  # get the same color
     elif (next, start) in list(colors.keys()):  # else if connection was visited, but from next to start
-        if colors[(next, start)] != 0:  # do the same
+        if colors[(next, start)] != 0:  # do the same but from next to start
             color = colors[(next, start)] - 20
             nx.set_edge_attributes(G, {(next, start): {'color': color}})
         else:
             color = colors[(next, start)]
-            nx.set_edge_attributes(G, {(next, start): {'color': color}})
     else:  # if its first visit from start to next (or from next to start)
         color = 120  # set light grey
         nx.set_edge_attributes(G, {(start, next): {'color': color}})
@@ -72,6 +70,6 @@ def getColor(G, colors, start, next):
 
 
 if __name__ == '__main__':
-    N = 20  # number of vertices in one dimension
-    n = 10  # number of steps
+    N = 20  # number of vertices in one dimension (number of vertices = NxN)
+    n = 2000  # number of steps
     initRandomWalk(n, N)
