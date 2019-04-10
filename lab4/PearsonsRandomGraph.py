3e181ae9aae2afa06e7b4c975f39493db3a4de2f
Paulina <34506586+Czudo@users.noreply.github.com>
2019-04-10 16:32:39 +0200

    plot random walk (density not work)

--- lab4/PearsonsRandomGraph.py
+++ lab4/PearsonsRandomGraph.py
@@ -2,14 +2,20 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import random
 import math
+import collections
+import numpy as np
+
+def getCords(G):
+    cords = collections.OrderedDict(sorted(nx.get_node_attributes(G, 'cords').items())).values()
+    cords = list(zip(*cords))
+    return cords
 
 
-def initPRandomWalk(n):
-    G = nx.Graph()
+def PearsonsRandomWalk(n, G):
     start = 0
     G.add_node(start, cords=[0, 0])
     x, y = 0, 0
-    plt.figure()
+
     for i in range(1, n+1):
         phi = random.random()*2*math.pi
         x = x + math.cos(phi)
@@ -18,16 +24,45 @@ def initPRandomWalk(n):
         G.add_edge(start, i, angle=phi)
         start = i
 
-        plt.plot()
-    #sort by key
-    cords = list(nx.get_node_attributes(G, 'cords').values())
-
-    cords = list(zip(*cords))
-    plt.plot(cords[0],cords[1], 'o')
+    cords = getCords(G)
+    plt.plot(cords[0], cords[1], '-o', linewidth=0.5, markersize=1)
+    return max(cords[0]), min(cords[0]), max(cords[1]), min(cords[1])
 
 
-    plt.show()
+def distribution(G, N):
+    An = [0]*len(G)
+    print(An)
+    Bn = [0]*len(G)
+    for i in range(len(G)):
+        x,y = getCords(G[i])
+        An[i] = sum(1 for k in x if k > 0)
+        Bn[i] = sum(1 for k in range(len(x)) if x[k] > 0 and y[k] > 0)
+    plt.figure()
+    [unique, counts] = np.unique(np.sort(An), return_counts=True)
+    plt.plot(unique, counts)
+    plt.savefig('An_{}.png'.format(N))
+    print(Bn)
+    plt.figure()
+    plt.hist(Bn, density=True, bins=10, label='empirical')
+    plt.savefig('Bn_{}.png'.format(N))
 
 
 if __name__ == '__main__':
-    initPRandomWalk(10000)
+    G = []
+    n = 2000
+    N = 1000
+    maxx, minx, maxy, miny = 0, 0, 0, 0
+    for i in range(0, n):
+        G.append(nx.Graph())
+        tempmaxx, tempminx, tempmaxy, tempminy = PearsonsRandomWalk(N, G[i])
+        maxx = max(maxx, tempmaxx)
+        minx = min(minx, tempminx)
+        maxy = max(maxy, tempmaxy)
+        miny = min(miny, tempminy)
+
+    plt.plot([minx, maxx], [0, 0], ':', linewidth=1, color='black')
+    plt.plot([0, 0], [miny, maxy], ':', linewidth=1, color='black')
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.savefig('PRandomWalk_{}_{}.png'.format(N, n))
+    distribution(G, N)
