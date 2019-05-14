3a9503af1c26377bcaf648fe997200756421b7d7
Paulina <cudo.paulina@gmail.com>
2019-05-14 13:29:57 +0200

    add fraction of infected nodes in the network at each time

--- lab5/SIRGraph.py
+++ lab5/SIRGraph.py
@@ -1,16 +1,12 @@
 import matplotlib.pyplot as plt
 import networkx as nx
-import math
 import random
 import subprocess
 import numpy as np
 import multiprocessing
 
 
-def SIRGraph(args):
-    name = args[0]
-    G = args[1]
-    p = args[2]
+def initSIR(G):
     # set to all nodes status 'susceptible'
     nx.set_node_attributes(G, 'S', 'status')
     nx.set_node_attributes(G, 'blue', 'color')
@@ -18,11 +14,29 @@ def SIRGraph(args):
     vertex = random.choice(list(G.nodes()))
     G.node[vertex]['status'] = 'I'
     G.node[vertex]['color'] = 'red'
+    p = [0.5, 0.7, 0.9]
+    multiprocess = multiprocessing.Pool()
+    for j in p:
+        graphs = [(G.copy(), j) for i in range(1, 10000)]
+        a = multiprocess.map(SIRGraph, graphs)
+        a = np.mean(np.asarray(a), axis=0)
+        plt.plot([i for i in range(0, len(a))], a, label=r'$p=$'+str(j))
+    plt.legend()
+    plt.show()
+
+
+def SIRGraph(args, name=None, plot=False):  # Graph and probability as tuple: (G,p)
+    G = args[0]
+    p = args[1]
     # list of nodes with 'status' 'infected'
-    infected = [x for x,y in G.nodes(data=True) if y['status']=='I']
-    step=0
-    pos = nx.spring_layout(G)
-    saveActualPlot(G, step, pos, name)
+    infected = [x for x, y in G.nodes(data=True) if y['status'] == 'I']
+    step = 0
+    infNodes = np.zeros(30)  # trza znaleźć złoty środek :C
+    infNodes[step] = len(infected)
+    if plot:
+        pos = nx.spring_layout(G)
+        saveActualPlot(G, step, pos, name)
+
     while infected:
         for i in infected:
             neighbours = list(G.neighbors(i))
@@ -34,8 +48,13 @@ def SIRGraph(args):
             G.node[i]['status'] = 'R'
             G.node[i]['color'] = 'grey'
         step = step+1
-        saveActualPlot(G, step, pos, name)
+        if plot:
+            saveActualPlot(G, step, pos, name)
         infected = [x for x, y in G.nodes(data=True) if y['status'] == 'I']
+        infNodes[step] = len(infected)
+    if plot:
+        createGIF(name)
+    return infNodes
 
 
 def saveActualPlot(G, step, pos, name):
@@ -50,19 +69,24 @@ def saveActualPlot(G, step, pos, name):
     fig1.savefig(str(name) + '/' + str(name) + '_{0:04}.png'.format(step))
     plt.close()
 
-def getRandomGraph():
+
+def createGIF(name):
+    cmd = ['magick', 'convert', '-delay', '40', '-loop', '0', str(name) + '/' + str(name) + '_*.png', str(name) + '/' + str(name) + '_gif.gif']
+    subprocess.call(cmd, shell=True)
+
+
+def getRandomGraphs(N, p):
     # [2d lattice, Erdos-Renyi, Watts–Strogatz, Barabassi-Albert]
-    return [('2d', nx.grid_graph(dim=[10, 10]), 0.6), ('er', nx.gnm_random_graph(100, 25), 0.6),
-            ('ws', nx.watts_strogatz_graph(100, 5, 0.3), 0.6), ('ba', nx.barabasi_albert_graph(100, 5), 0.6)]
+    # lepsze parametry dla grafów (by ładniejsze wykresy były)
+    return [('2d', nx.grid_graph(dim=[int(np.sqrt(N)), int(np.sqrt(N))]), p),
+            ('er', nx.gnm_random_graph(N, 400), p),
+            ('ws', nx.watts_strogatz_graph(N, 4, 0.3), p),
+            ('ba', nx.barabasi_albert_graph(N, 3), p)]
 
 
 if __name__ == '__main__':
     N = 100
-    name = ['2d', 'er', 'ws', 'ba']
-    G = getRandomGraph()
+    G = nx.grid_graph(dim=[int(np.sqrt(N)), int(np.sqrt(N))])
 
-    #SIRGraph([G[0],G[1],0.6])
-    # that two line is for doing SIRGraph concurent for every type of graph
-    p = multiprocessing.Pool()
-    a = p.map(SIRGraph, G)
-    #print(a)
+    initSIR(G)
+
